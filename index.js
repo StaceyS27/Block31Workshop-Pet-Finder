164746f67eb16430fc10364082138160c1be2403
Stacey Sanchez <staceysanchez@macbook-air.mynetworksettings.com>
2023-10-11 17:58:43 -0400

    api routes/codes written

--- index.js
+++ index.js
@@ -5,13 +5,17 @@ const pets = require('./data');
 const express = require('express');
 const app = express();
 
-const PORT = 8080;
+const PORT = 5175;
+
+const morgan = require('morgan');
+app.use(morgan('dev'));
 
 // GET - / - returns homepage
 app.get('/', (req, res) => {
     // serve up the public folder as static index.html file
+    res.sendFile(__dirname + "/public/index.html");
 
-});
+})
 
 // hello world route
 app.get('/api', (req, res) => {
@@ -21,30 +25,36 @@ app.get('/api', (req, res) => {
 // get all pets from the database
 app.get('/api/v1/pets', (req, res) => {
     // send the pets array as a response
+    res.send(pets);
+    console.log(pets);
 
 });
 
 // get pet by owner with query string
 app.get('/api/v1/pets/owner', (req, res) => {
     // get the owner from the request
+    const owner = req.query.owner;
 
 
     // find the pet in the pets array
     const pet = pets.find(pet => pet.owner === owner);
 
     // send the pet as a response
+    res.send(pet)
+    console.log(pet);
 
 });
 
 // get pet by name
 app.get('/api/v1/pets/:name', (req, res) => {
     // get the name from the request
-
+    const name = req.params.name
 
     // find the pet in the pets array
     const pet = pets.find(pet => pet.name === name);
 
     // send the pet as a response
+    res.send(pet);
 
 });
 
